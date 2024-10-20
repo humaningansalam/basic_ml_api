c35f9253da9b0ff4693694446ad53220e994b599
sanghun9634 <sanghun9634@gmail.com>
2024-10-21 00:01:44 +0900

    feat: health check test 코드 추가

---
+++ myapp/tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from main import app as flask_app
+
+@pytest.fixture
+def app():
+    """Flask 애플리케이션을 반환하는 fixture"""
+    return flask_app
+
+@pytest.fixture
+def client(app):
+    """Flask 테스트 클라이언트를 반환하는 fixture"""
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    """Flask CLI runner를 반환하는 fixture"""
+    return app.test_cli_runner()
